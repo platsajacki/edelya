27182517fa5ccd0df1b532d3c3297ad0d47b4b5d
platsajacki <menyukhov@bk.ru>
2026-03-22 19:13:11 +0300

    fix(settings): add CSRF_TRUSTED_ORIGINS environment variable handling

--- src/core/settings.py
+++ src/core/settings.py
@@ -16,6 +16,7 @@ DEBUG = bool(int(getenv('DEBUG', 0)))
 ALLOWED_HOSTS = getenv('ALLOWED_HOSTS', '').split(', ')
 CORS_ALLOWED_ORIGINS = getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:5173, http://127.0.0.1:5173').split(', ')
 CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = getenv('CSRF_TRUSTED_ORIGINS', 'http://127.0.0.1').split(', ')
 
 DJANGO_APPS = [
     'django.contrib.admin',
