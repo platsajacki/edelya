e5f4009af17b082965a9442f084abd266a8a0714
platsajacki <menyukhov@bk.ru>
2026-05-11 15:07:38 +0300

    feat(celery): add namespace to Celery config for improved settings management

--- src/core/celery.py
+++ src/core/celery.py
@@ -7,7 +7,7 @@ from celery import Celery, signals
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 celery_app = Celery('core')
-celery_app.config_from_object('django.conf:settings')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
 
 
